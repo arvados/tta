85f199358f5dc317f86ad4b2de84f2add26c1b48
Davide Fiorentino lo Regio <davide.fiorentino.loregio@gmail.com>
2015-03-17 22:08:14 +0100

    Update debian.sh
    Snapshot #2

--- debian.sh
+++ debian.sh
@@ -5,3 +5,20 @@ sudo apt-get install \
     libcurl4-openssl-dev libpcre3-dev libpq-dev libreadline-dev \
     libssl-dev libxslt1.1 postgresql git wget zlib1g-dev
 ### 1
+
+## Install Ruby and Bundler
+pushd $HOME
+	mkdir -p src
+	pushd src
+		wget http://cache.ruby-lang.org/pub/ruby/2.1/ruby-2.1.5.tar.gz
+		tar xzf ruby-2.1.5.tar.gz
+		cd ruby-2.1.5
+		./configure
+		make
+		sudo make install
+		sudo gem install bundler
+	popd
+	rm -rf src/ruby-2.1.5
+popd
+git clone https://github.com/curoverse/arvados.git
+### 2
