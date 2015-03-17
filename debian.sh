d605455114105bd468be868dbe3ff494bee7c9b3
Davide Fiorentino lo Regio <davide.fiorentino.loregio@gmail.com>
2015-03-17 22:07:47 +0100

    Add debian.sh
    snapshot #1

---
+++ debian.sh
@@ -0,0 +1,7 @@
+#/bin/bash
+# Install dependencies
+sudo apt-get install \
+    bison build-essential gettext libcurl3 libcurl3-gnutls \
+    libcurl4-openssl-dev libpcre3-dev libpq-dev libreadline-dev \
+    libssl-dev libxslt1.1 postgresql git wget zlib1g-dev
+### 1
